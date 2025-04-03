2523d5e155423f3914c4d13eafbd9895c77b77b8
Francisco Casillas <chiccasillas@gmail.com>
2025-04-03 14:59:05 -0600

    Initial Commit

---
+++ TravelGuideApp/TravelGuideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGuideAppApp.swift
+//  TravelGuideApp
+//
+//  Created by Francisco Jean on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelGuideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
