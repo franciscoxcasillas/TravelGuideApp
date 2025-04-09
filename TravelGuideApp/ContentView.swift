5aec9fdd54e2f8c38841fb17f124e8250c13a1dd
Francisco Casillas <chiccasillas@gmail.com>
2025-04-09 13:31:34 -0600

    Create DestinationCardView and DestinationViews. Add assets and images. Fix layout.

--- TravelGuideApp/ContentView.swift
+++ TravelGuideApp/ContentView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+struct Destination: Identifiable {
+    let id = UUID()
+    let name: String
+    let location: String
+    let imageName: String
+}
+
+
 struct ContentView: View {
     var body: some View {
         NavigationView {
@@ -17,20 +25,88 @@ struct ContentView: View {
                     .edgesIgnoringSafeArea(.all)
                 
                 VStack {
+                    Spacer()
                     Text("Welcome to Travel Guide App")
                         .font(.custom("Montserrat-Bold", size: 20))
-                    Image(systemName: "airplane")
+                        .foregroundColor(.white)
+                    Image("plane2")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 230)
                     Spacer().frame(height: 30)
-                    NavigationLink(destination: ContentView()) {
-                        Text("Profile")
+                    NavigationLink(destination: DestinationViews()) {
+                            Label("Destinations", systemImage: "airplane")
                             .foregroundColor(.white)
-                            .underline()
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(10)
+                        
+                        }
+                    Spacer()
+                    Spacer()
                     }
                 }
             }
         }
     }
+
+
+struct DestinationViews: View {
+    
+    let destinations = [
+        Destination(name: "Paris", location: "France", imageName: "paris"),
+        Destination(name: "Rome", location: "Italy", imageName: "roma"),
+        Destination(name: "Tokyo", location: "Japan", imageName: "tokyo"),
+        Destination(name: "New York", location: "USA", imageName: "newyork"),
+        Destination(name: "London", location: "UK", imageName: "london"),
+        Destination(name: "Sydney", location: "Australia", imageName: "sydney")
+    ]
+    
+    let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+        
+        var body: some View {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(destinations) { destination in
+                        DestinationCardView(destination: destination)
+                    }
+                }
+                .padding()
+            }
+        }
 }
+
+struct DestinationCardView: View {
+    let destination: Destination 
+
+    var body: some View {
+        VStack (alignment: .center) {
+            Image(destination.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+            HStack {
+                VStack() {
+                    Text(destination.name)
+                        .font(.headline)
+                    Text(destination.location)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.gray)
+                }
+            }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+}
+        
 
 
 
